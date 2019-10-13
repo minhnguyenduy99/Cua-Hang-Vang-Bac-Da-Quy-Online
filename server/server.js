b37d89486c6f0ebf1f88727607f4427957c47051
minhnguyenduy99 <minhnguyenduy99@gmail.com>
2019-10-13 21:09:38 +0700

    update server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
-const DBConnection = require('./src/dbConnection');
+const conn = require('./src/dbConnection');
+
 const app = require('./src/app');
 const config = require('./config');
+const mysql = require('mysql');
 
-
-
-const db = new DBConnection(config.dbConnectConfig);
-db.connect();
+conn.connect(err => {
+    if (err) {
+        console.log('Connect to database failed')
+    }
+    else{
+        console.log('Connect to database successfully')
+    }
+})
 
 app.listen(3000, () => {
     console.log(`The server is running...`);
--- server/src/app.js
+++ server/src/app.js
@@ -3,10 +3,30 @@ const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
+const userRoute = require('./routes/api/users');
+
 // middlewares
 app.use(bodyParser.urlencoded({extended : false}));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
+
+
+app.use('/users', userRoute);
+
+
+app.use((req, res, next) => {
+    const err = new Error('Not found');
+    err.status(404);
+    next(err);
+})
+
+app.use((err, req, res, next) => {
+    res.status(err.status | 500);
+
+    res.json({
+        message: err.message
+    })
+})
 
 
 module.exports = app;
